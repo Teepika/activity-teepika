afc35ac327f3e50c6abe6540eb1bbbbbbea130c3
SivashangariSivakumaran <sivashangari1998@gmail.com>
2023-10-14 23:20:20 +0530

    Implemented a function for adding two numbers

---
+++ add-two-numbers.py
@@ -0,0 +1,15 @@
+def add_numbers(a, b):
+    """
+    This function takes two numbers and returns their sum.
+
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+
+    Returns:
+    int: The sum of a and b.
+    """
+    return a + b
+
+result = add_numbers(5, 10)
+print("Sum:", result)  # Output: Sum: 15
